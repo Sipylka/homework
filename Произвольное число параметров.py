4706247a0301e465c615c79504279057aa533652
Spiylka <shataev.artur@mail.ru>
2024-12-16 02:00:29 +0300

    выполнил Самостоятельная работа по уроку "Распаковка позиционных параметров"

---
+++ Произвольное число параметров.py
@@ -0,0 +1,7 @@
+def summator (txt, *values, type = 'sum'):
+    s = 0
+    for i in values:
+        s += i
+    return f'{txt}{s} {type}'
+
+print(summator('Сумма чисел: ', 2, 3, 4, type = 'summator'))
