6d2f2b1fe01917e437428c422155708ee99dd068
Spiylka <shataev.artur@mail.ru>
2024-12-15 10:45:34 +0300

    выполняю задание по рассылке писем

--- Рассылка писем.py
+++ Рассылка писем.py
@@ -1,14 +1,24 @@
-def send_email(message, recipient, sender = "university.help@gmail.com"):
-    x = str(recipient)
-    if not (not '@' or '.ru' or '.com' or '.net' in recipient):
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-    elif recipient == sender:
-        print('Нельзя отправить письмо самому себе!')
-    elif sender == 'university.help@gmail.com':
-        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
-    else:
-        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
 
+    # Проверка на корректность email-адресов
+    valid_domains = (".com", ".ru", ".net")
+    if "@" not in sender or not sender.endswith(valid_domains) or "@" not in recipient or not recipient.endswith(valid_domains):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
+
+    # Проверка на отправку самому себе
+    if recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+        return
+
+    # Проверка на отправителя по умолчанию
+    if sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+# Примеры вызова функции
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
--- Черновик.py
+++ Черновик.py
@@ -1,10 +1,13 @@
+sender = "university.help@gmail.com"
 x = 'vasyok1337@gmail.com'
-i = x.find('@')
-print(len(x), i, x[0:i])
-if x.find('@') != -1 and x[-4:].find('.ru') != -1 or x[-4:].find('.com') != -1 or x[-4:].find('.net') != -1:
-    print('есть')
-else:
+#if not ('.ru' or '.com' or '.net') in x:
+if '@' in x :
+    print(x)
+    print(f'Невозможно отправить письмо с адреса {sender} на адрес {x}')
     print('нет')
-print(x[-4:])
+else:
+    print(x)
+    print('есть')
+    #print(sender.find('@'))
 
 #send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
